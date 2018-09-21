65b726ab8ddea824fba3e98f3f967c11e61bbca4
Maxime Barber <maximebarber@Maximes-MacBook-Air.local>
2018-09-21 22:46:33 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,74 +1,48 @@
 Vue.use(vuelidate.default)
 
-const pizzaOrBurger = value => value === 'pizza' || value === 'burger' || !validators.helpers.req(value)
-const oldEnoughAndAlive = validators.between(12, 120)
+new Vue
+({
+    el: '#app',
 
-new Vue({
-  el: '#app',
-
-  data () {
-    return {
-      form: {
-        name: null,
-        age: null,
-        email: null,
-        newsletter: null,
-        githubUsername: null,
-        food: null
-      }
-    }
-  },
-
-  validations: {
-    form: {
-      name: {
-        required: validators.required
-      },
-
-      age: {
-        required: validators.required, // $v.form.age.required
-        integer: validators.integer, // $v.form.age.integer
-        oldEnoughAndAlive // $v.form.age.oldEnoughAndAlive
-      },
-
-      email: {
-        email: validators.email,
-        required: validators.requiredIf(function () {
-          return !!this.form.newsletter
-        })
-      },
-
-      githubUsername: {
-        exists (value) {
-          if (!validators.helpers.req(value)) {
-            return true
-          }
-          return axios.get(`//api.github.com/users/${value}`)
+    data () 
+    {
+        return {
+            form: 
+            {
+                name: null,
+                age: null,
+            }
         }
-      },
-
-      food: {
-        pizzaOrBurger
-      }
-    }
-  },
-
-  methods: {
-    shouldAppendValidClass (field) { // ex: field = $v.form.email
-      return !field.$invalid && field.$model && field.$dirty
     },
 
-    shouldAppendErrorClass (field) { // ex: field = $v.form.email
-      return field.$error
+    // Vuelidate validations
+    validations: 
+    {
+        form: 
+        {
+            name: 
+            {
+                required: validators.required
+            },
+
+            age: 
+            {
+                required: validators.required,
+                integer: validators.integer, 
+                between: validators.between(12, 120),
+            },
+        }
     },
 
-    submitForm () {
-      this.$v.form.$touch()
-      if (!this.$v.form.$invalid) {
-        console.log('📝 Form Submitted', this.form)
-      } else {
-        console.log('❌ Invalid form')
-      }
+    methods: 
+    {
+        submitForm() 
+        {
+            // if form is not invalid
+            if (!this.$v.form.$invalid) 
+              console.log('📝 Form Submitted', this.form)
+            else 
+              console.log('❌ Invalid form')
+        }
     }
-  }
 })
