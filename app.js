87247a1c0c9c20fe73d6b105ea59af192541212c
Maxime Barber <maximebarber@Maximes-MacBook-Air.local>
2018-09-22 21:20:03 +0200

    Finished form-validation

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 Vue.use(vuelidate.default)
 
+const oldEnoughAndAlive = validators.between(12, 120)
+
 new Vue
 ({
     el: '#app',
@@ -11,6 +13,10 @@ new Vue
             {
                 name: null,
                 age: null,
+                email: null,
+                newsletter: null,
+                food: null,
+                githubUsername: null,
             }
         }
     },
@@ -27,17 +33,60 @@ new Vue
 
             age: 
             {
+                // $v.form.age.required
                 required: validators.required,
+                // $v.form.age.integer
                 integer: validators.integer, 
-                between: validators.between(12, 120),
+                // $v.form.age.oldEnoughAndAlive
+                oldEnoughAndAlive
+            },
+
+            email:
+            {
+                email: validators.email,
+                required: validators.requiredIf(function ()
+                {
+                    return !!this.form.newsletter
+                })
+            },
+
+            githubUsername:
+            {
+                exists(value)
+                {
+                    if(!validators.helpers.req(value))
+                    {
+                        return true
+                    }
+                    return axios.get(`//api.github.com/users/${value}`)
+                }
+            },
+
+            food:
+            {
+                pizzaOrBurger: value => value === 'pizza' || value === 'burger' || !validators.helpers.req(value)
             },
         }
     },
 
     methods: 
     {
+        // Logic of when to append 'valid' class
+        shouldAppendValidClass(field)
+        {
+            return !field.$invalid && field.$model && field.$dirty
+        },
+
+        // Logic of when to append 'error' class
+        shouldAppendErrorClass(field)
+        {
+            return  field.$error
+        },
+
         submitForm() 
         {
+            // Touch all the properties under the form object
+            this.$v.form.$touch()
             // if form is not invalid
             if (!this.$v.form.$invalid) 
               console.log('📝 Form Submitted', this.form)
